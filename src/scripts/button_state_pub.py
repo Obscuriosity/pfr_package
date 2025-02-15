2ff2ab793fbb4bc7996b3c86c19e909b23f12bea
Obscuriosity <tony.obscuriosity@gmail.com>
2025-02-15 21:18:04 +0000

    Added Button state publisher

---
+++ src/scripts/button_state_pub.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+
+# Author:   Tony Willett
+# Date:     15 2 25
+# Description: Node to monitor Dilyn's buttons and publish state
+
+import RPi.GPIO as GPIO
+import rospy
+from std_msgs.msg import Bool
+
+class buttonStatePublisher:
+
+    def __init__(self):
+        # Button Pins
+        self.red_but_pin = 18
+        self.yel_but_pin = 16
+        self.gre_but_pin = 22
+        self.blu_but_pin = 24
+        # Button state log
+        self.red_but_state = False
+        self.yel_but_state = False
+        self.gre_but_state = False
+        self.blu_but_state = False
+
+        # Ros Publishers
+        self.redButtonPub = rospy.Publisher('redButtonState', Bool, queue_size = 1)
+        self.yelButtonPub = rospy.Publisher('yelButtonState', Bool, queue_size = 1)
+        self.greButtonPub = rospy.Publisher('greButtonState', Bool, queue_size = 1)
+        self.bluButtonPub = rospy.Publisher('bluButtonState', Bool, queue_size = 1)
+
+        self.GPIO.setmode(GPIO.BOARD)
+        self.GPIO.setwarnings(False)
+        self.GPIO.setup(self.red_but_pin, GPIO.IN)
+        self.GPIO.setup(self.yel_but_pin, GPIO.IN)
+        self.GPIO.setup(self.gre_but_pin, GPIO.IN)
+        self.GPIO.setup(self.blu_but_pin, GPIO.IN)
+
+        self.GPIO.add_event_detect(self.red_but_pin, GPIO.FALLING, callback = redButtonCB, bouncetime = bounce)
+        self.GPIO.add_event_detect(self.yel_but_pin, GPIO.FALLING, callback = yelButtonCB, bouncetime = bounce)
+        self.GPIO.add_event_detect(self.gre_but_pin, GPIO.FALLING, callback = greButtonCB, bouncetime = bounce)
+        self.GPIO.add_event_detect(self.blu_but_pin, GPIO.FALLING, callback = bluButtonCB, bouncetime = bounce)
+
+        def redButtonCB(channel):
+            self.red_but_state = not self.red_but_state
+            self.redButtonPub(self.red_but_state)
+
+        def yelButtonCB(channel):
+            self.yel_led_state = not self.yel_led_state
+            self.yelButtonPub(self.yel_led_state)
+
+        def greButtonCB(channel):
+            self.gre_led_state = not self.gre_led_state
+            self.greButtonPub(self.gre_led_state)
+
+        def bluButtonCB(channel):
+            self.blu_led_state = not self.blu_led_state
+            self.bluButtonPub(self.blu_led_state)
+
+
+def main():
+    rospy.init_node('button_state_node')
+    rospy.loginfo("Button Monitor started")
+    bsp = buttonStatePublisher()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
